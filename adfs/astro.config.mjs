f89e9249ac429931305efc679997ca5f8b863ed6
Dhuke Chike <dukelchike31@gmail.com>
2026-04-28 01:49:56 +0100

    added node adapter for astro

--- adfs/astro.config.mjs
+++ adfs/astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import vue from "@astrojs/vue";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+	output: "server",
+	adapter: node({
+		mode: "standalone", // or "middleware"
+	}),
 	devToolbar: { enabled: false },
 	integrations: [
 		vue({
