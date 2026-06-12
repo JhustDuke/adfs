e12b5c41e164d8df4f3de2b3b6357f4f88f8d42a
Dhuke Chike <dukelchike31@gmail.com>
2026-06-12 17:22:21 +0100

    staff api working moving on

--- adfs/src/pages/urls.ts
+++ adfs/src/pages/urls.ts
@@ -1,7 +1,7 @@
 export const urlStringObj = {
 	newsUrl: "/api/news/getAllNews",
 	academicUrl: "/api/academic/getAllAcademicCard",
-	galleryUr: "/api/gallery/getAllGallery",
-	staffsUrl: "/api/staff/getAllStaffs",
+	galleryUrl: "/api/gallery/getAllGallery",
+	staffUrl: "/api/staff/getAllStaffs",
 	aboutUrl: "/api/about/getAllAboutCard",
 };
