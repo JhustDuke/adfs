36b6a1d383111305c5fd3e40c6256f4ec84b341c
Dhuke Chike <dukelchike31@gmail.com>
2026-06-12 11:23:37 +0100

    feat: used ssr in the academics page, created an url object for all urls

---
+++ adfs/src/pages/urls.ts
@@ -0,0 +1,7 @@
+export const urlStringObj = {
+	newsUrl: "/api/news/getAllNews",
+	academicUrl: "/api/academic/getAllAcademicCard",
+	galleryUr: "/api/gallery/getAllGallery",
+	staffsUrl: "/api/staff/getAllStaffs",
+	aboutUrl: "/api/about/getAllAboutCard",
+};
